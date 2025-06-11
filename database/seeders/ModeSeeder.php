afa6b16555c3f191e182d4863a9cb5cff2de90e4
Juan <lopezsurianjuancarlos5@gmail.com>
2025-06-11 09:36:42 -0600

    primer commit

---
+++ database/seeders/ModeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ModeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
